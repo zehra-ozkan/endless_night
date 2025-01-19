b90f58241dccbcdb9b98e1a2c36f4bdc37615026
zehra-ozkan <zehra.ozkan138@gmail.com>
2025-01-19 21:22:05 +0300

    sckeleton complete, needs to be centered, characters menu needs work

--- src/main/java/com/dilazehra/endless_night/App.java
+++ src/main/java/com/dilazehra/endless_night/App.java
@@ -18,12 +18,12 @@ public class App extends Application {
         //FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/com/dilazehra/Scenes/enranceScene.fxml"));
         FXMLLoader fxmlLoader = FxmlLoader.load("enranceScene");
         Scene scene = new Scene(fxmlLoader.load());
-        stage.setWidth(800);   // Optional: Set the stage size
-        stage.setHeight(600);  // Optional: Set the stage size
+//        stage.setWidth(800);   // Optional: Set the stage size
+//        stage.setHeight(600);  // Optional: Set the stage size
         stage.setScene(scene);
         stage.show();
     }
-
+//TODO SETUP IS NECESSARRY INITIAL SETUP IS WRONG!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
     public static void main(String[] args) {
         launch();
     }
